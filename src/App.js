65c1fde46fbb2eaef90db7934e4cb66fe65b93f2
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2023-01-28 18:22:50 +0300

    addComponent-Row

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import requests from "./requests";
+import Row from "./Row";
+import "./reset.css"
 
 function App() {
   return (
     <div className="App">
     NETFLİX APP YOU ARE WELCOME
 
-    {/* <Row /> */}
+    <Row title={"Trending Now"} fetchURL={requests.fetchTrending} />
+    <Row title={"Netflix Original"} fetchURL={requests.fetchNetflixOriginals} />
+    <Row title={"Top Rated"} fetchURL={requests.fetchTopRated} />
+    <Row title={"Action Movies"} fetchURL={requests.fetchActionMovies} />
+    <Row title={"Comedy Movies Now"} fetchURL={requests.fetchComedyMovies} />
+    <Row title={"Horror Movies"} fetchURL={requests.fetchHorrorMovies} />
+    <Row title={"Romance Movies"} fetchURL={requests.fetchRomanceMovies} />
+    <Row title={"Documentaries Movies"} fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
