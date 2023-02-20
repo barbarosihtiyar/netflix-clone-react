5dbfcbd50851035dc537a86d8a7ba6de726a5084
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2023-02-20 21:59:39 +0300

    registerForm&addUseFormik

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useFormik } from 'formik';
+import { Link } from 'react-router-dom';
 
-function SignUp() {
+function Signup() {
   return (
-    <div>SignUp</div>
+    <main>
+      <section className="signup">
+        <div className="signupTitle">
+          <h1>Register</h1>
+        </div>
+        <div className="signupForm">
+          <form>
+            <label htmlFor='name'>Name and Surname</label>
+            <input name='name' id='name' type="text" placeholder='Name and Surname'/>
+
+            <label htmlFor='username'>Username</label>
+            <input name='username' id='username' type="text" placeholder='Username'/>
+
+
+            <label htmlFor='email'>Email Adress</label>
+            <input name='email' id='email' type="text" placeholder='Email Adress'/>
+
+            <label htmlFor='password'>Password</label>
+            <input name='password' id='password' type="password" placeholder='password'/>
+
+            <label htmlFor='passwordAgain'>Password Again</label>
+            <input name='passwordAgain' id='passwordAgain' type="password" />
+
+            <div className="checkbox">
+              <input type="checkbox" name='condition' id='condition' />
+              <label htmlFor='condition'>By ticking this box, you will be deemed to have accepted the terms of use.</label>
+            </div>
+            <button type='submit'>CREATE ACCOUNT</button>
+          </form>
+          <span>Already have an account <Link to="/">Sign In</Link></span>
+        </div>
+      </section>
+    </main>
   )
 }
 
-export default SignUp
+export default Signup
