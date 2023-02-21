4218d9a2299783b67e699c4842bf20e270161c88
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2023-02-21 22:35:14 +0300

    createdForm

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
+import "../sass/signup.css";
+import netflixLogo from "../images/Netflix-Logo-.webp";
 
 function Signup() {
   return (
     <main>
       <section className="signup">
+      <div className="signupContainer">
+        <div className="signupWrapper">
+        <div className="registerHeader">
+              <img src={netflixLogo} alt="" />
+            </div>
         <div className="signupTitle">
-          <h1>Register</h1>
+          <h1>CREATE ACCOUNT</h1>
         </div>
         <div className="signupForm">
           <form>
@@ -22,10 +29,26 @@ function Signup() {
             <input name='email' id='email' type="text" placeholder='Email Adress'/>
 
             <label htmlFor='password'>Password</label>
-            <input name='password' id='password' type="password" placeholder='password'/>
+            <input name='password' id='password' type="password" placeholder='Password'/>
 
             <label htmlFor='passwordAgain'>Password Again</label>
-            <input name='passwordAgain' id='passwordAgain' type="password" />
+            <input name='passwordAgain' id='passwordAgain' type="password" placeholder='Password Again' />
+
+            <label htmlFor='birthday'>Birthday</label>
+            <input name='birthday' id='birthday' type="date" placeholder='Password Again' />
+
+            <label htmlFor='gender'>Gender</label>
+            <div className="malefemale">
+            <div className="male">
+            <input name='gender' id='male' type="radio"  />
+            <label htmlFor='male'>Male</label>
+            </div>
+            <div className="female">
+            <input name='gender' id='female' type="radio"  />
+            <label htmlFor='female'>Female</label>
+            </div>
+            </div>
+
 
             <div className="checkbox">
               <input type="checkbox" name='condition' id='condition' />
@@ -35,6 +58,8 @@ function Signup() {
           </form>
           <span>Already have an account <Link to="/">Sign In</Link></span>
         </div>
+        </div>
+      </div>
       </section>
     </main>
   )
