811ed11ef1da95c298c1869e8d2b3f77b31c1e99
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-03-11 11:41:53 +0300

    pageAccountHtml

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,77 +1,119 @@
-import React from 'react'
-import Header from './Header'
-import "../sass/account.css"
+import React from "react";
+import Header from "./Header";
+import "../sass/account.css";
 
 function Account() {
   return (
-    <>
-    <div className="headerComponent">
-    <Header />
-    </div>
-    <div className="emptyRow"></div>
-    <div className="accountContainer">
-      <div className="accountWrapper">
-        <div className="account">
-          <div className="accountTitle">
-            <h3>Account</h3>
-          </div>
-          <div className="accountInfo">
-            <div className="membershipPayments">
-            <div className="membershipPaymentsFirst">
-              <div className="membershipPaymentsTitleButton">
-                <h6>MEMBERSHIP AND PAYMENTS</h6>
-                <button type='button'>Cancel Membership</button>
-              </div>
-              <div className="userInfo">
-                <span>aaaa@hotmail.com</span>
-                <span>Password : asdasdas</span>
-                <span>Phone : 111111111</span>
-              </div>
-              <div className="changeUserInfo">
-                <span>Change account email</span>
-                <span>Change password</span>
-                <span>Change phone number</span>
-              </div>
-              </div>
-              <div className="membershipPaymentsSecond">
-              <div className="userCardInfo">
-                <span>11111111111111111</span>
-              </div>
-              <div className="userCardInfoChange">
-                <span>Manage payment information</span>
-                <span>Add a backup payment method</span>
-                <span>Use a gift card or promotion</span>
-              </div>
-              </div>
+    <div className="accountComponent">
+      <div className="headerComponent">
+        <Header />
+      </div>
+      <div className="emptyRow"></div>
+      <div className="accountContainer">
+        <div className="accountWrapper">
+          <div className="account">
+            <div className="accountTitle">
+              <h3>Account</h3>
             </div>
-            <div className="planDetails">
-              <div className="planDetailsTitle">
-              <h6>PLAN DETAILS</h6>
+            <div className="accountInfo">
+              <div className="membershipPayments">
+                <div className="membershipPaymentsFirst">
+                  <div className="membershipPaymentsTitleButton">
+                    <h6>MEMBERSHIP AND PAYMENTS</h6>
+                    <button type="button">Cancel Membership</button>
+                  </div>
+                  <div className="userInfo">
+                    <span>aaaa@hotmail.com</span>
+                    <span>Password : asdasdas</span>
+                    <span>Phone : 111111111</span>
+                    <div className="userCardInfo">
+                      <span>11111111111111111</span>
+                    </div>
+                  </div>
+                  <div className="changeUserInfo">
+                    <span>Change account email</span>
+                    <span>Change password</span>
+                    <span>Change phone number</span>
+                    <div className="userCardInfoChange">
+                      <span>Manage payment information</span>
+                      <span>Add a backup payment method</span>
+                      <span>Use a gift card or promotion</span>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className="planType">
-                <span>Basic</span>
+              <div className="planDetails">
+                <div className="planDetailsTitle">
+                  <h6>PLAN DETAILS</h6>
+                </div>
+                <div className="planType">
+                  <span>Basic</span>
+                </div>
+                <div className="changePlan">
+                  <span>Change plan</span>
+                </div>
               </div>
-              <div className="changePlan">
-                <span>Change plan</span>
+              <div className="securityPrivacy">
+                <div className="securityPrivacyTitle">
+                  <h6>SECURITY AND PRIVACY</h6>
+                </div>
+                <div className="securityPrivacyDesc">
+                  <span>
+                    Control who can access this account, view recently used
+                    devices, and more.
+                  </span>
+                </div>
+                <div className="closeAccount">
+                  <span>Sign out on all devices</span>
+                </div>
               </div>
-            </div>
-            <div className="securityPrivacy">
-            <div className="securityPrivacyTitle">
-              <h6>SECURITY AND PRIVACY</h6>
+              <div className="profileParentalControls">
+                <div className="profileParentalControlsTitle">
+                  <h6>PROFILE AND PARENTAL CONTROLS</h6>
+                </div>
+                <div className="profileParentalControlsInfo">
+                  <span>asd</span>
+                </div>
               </div>
-            <div className="securityPrivacyDesc">
-              <span>Control who can access this account, view recently used devices, and more.</span>
-            </div>
-            <div className="closeAccount">
-              <span>Sign out on all devices</span>
-            </div>
+              <div className="settings">
+                <div className="settingTitle">
+                  <h6>Settings</h6>
+                </div>
+                <div className="settingsInfo">
+                  <span>18+ Content Access PIN</span>
+                  <span>Turn off profile transfer feature</span>
+                  <span>Test participation</span>
+                  <span>Manage content download devices</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+      <footer>
+        <div className="footerContainer">
+          <div className="footerWrapper">
+            <div className="footerAskQuestion">
+              <span>Have questions? Call us.</span>
+            </div>
+            <div className="footerPart">
+              <ul>
+                <li>Audio and Subtitles</li>
+                <li>Help center</li>
+                <li>Gift Cards</li>
+                <li>Investor Relations</li>
+                <li>Media Center</li>
+                <li>Job oppurtunities</li>
+                <li>Cookie Preferences</li>
+                <li>Terms of Use</li>
+                <li>Privacy Statement</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
-    </>
-  )
+  );
 }
 
-export default Account
+export default Account;
