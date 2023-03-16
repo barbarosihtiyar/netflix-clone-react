975639a52001f8caabdaf15085533ab413abadb0
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-03-16 20:41:24 +0300

    deletedEmpty

--- src/pages/ProfileManagement.js
+++ src/pages/ProfileManagement.js
@@ -1,11 +1,9 @@
 import React from 'react'
 
 function ProfileManagament() {
-    return (
-        <div>ProfileManagament</div>
-    )
+  return (
+    <div></div>
+  )
 }
 
 export default ProfileManagament
-
-
