fd339e05284b072af287b1324eee65455e010e24
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-03-24 21:37:58 +0300

    changeEditValFunc

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import userFirstLogo from "../images/userFirstLogo.png"
 import userSecondLogo from "../images/userSecondLogo.png"
 import userChild from "../images/userChild.png"
@@ -9,14 +9,18 @@ import { Link } from 'react-router-dom'
 
 
 function LandingPage() {
-  let {edit} = useContext(MainContext);
-
- 
+  let {edit,setEdit} = useContext(MainContext);
+  const [which,setWhich] = useState(true);
+  
     console.log(edit)
  
     let {subsInfo} = useContext(MainContext);
 
     const capitalizedWord = subsInfo.username.charAt(0).toUpperCase() + subsInfo.username.slice(1);
+
+    const changeWhich = () => {
+      setWhich(!which);
+    }
 
   return (
    <>
@@ -27,9 +31,12 @@ function LandingPage() {
         <h6 className='landingPageTitle'>Who is watching?</h6>
         <div className="userLogos">
         <div className="userLogoFirst usersLogo">
-        {edit ? <img className={edit ? " downOpacity" : ""} src={userFirstLogo} alt="" /> : <Link to="/homepage"><img className={edit ? " downOpacity" : ""} src={userFirstLogo} alt="" /></Link>}
+        <div className="" style={{margin:"auto"}}>
+        {edit ? <img className={edit ? " downOpacity" : ""} src={userFirstLogo} alt="" /> : <Link to="/home"><img className={edit ? " downOpacity" : ""} src={userFirstLogo} alt="" /></Link>}
         { edit ? <Link className='editSvg' to="/profilemanagement"><FiEdit2 /></Link> : ""}
+        </div>
         <span>{capitalizedWord}</span>
+       <span id='titleProfileManagement1'><Link to="/profilemanagement">Profile Management</Link></span>
         </div>
         {/* <div className="userLogoSecond usersLogo">
         <img src={userSecondLogo} alt="" />
