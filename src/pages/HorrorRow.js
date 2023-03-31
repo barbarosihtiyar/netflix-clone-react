2656417ef0ad734d6b8d9198be13eb2c64a84e76
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-03-31 21:48:51 +0300

    searchMovieHorrorComponent

--- src/pages/HorrorRow.js
+++ src/pages/HorrorRow.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import "../sass/horror.css"
 import axios from "../axios"
+import { MainContext, useContext } from "../context";
+
 
 function HorrorRow({title,fetchURL}) {
   const [horror,setHorror] = useState([])
+  let {change,setChange} = useContext(MainContext);
+
   useEffect(() => {
     const fetchHor = async () => {
       const horrorMovies = await axios.get(fetchURL)
@@ -13,13 +17,16 @@ function HorrorRow({title,fetchURL}) {
     console.log(horror)
   },[])
   const base_URL = "https://image.tmdb.org/t/p/original";
+
+  const filteredWordsHorror = horror.filter(word => word.original_title.toLowerCase().includes(change))
+
   
   return (
     <div className="horrorRowContainer">
         <div className="horrorRowWrapper">
             <h1>{title}</h1>
             <div className="horror">
-            {horror.map((movie,index) => (
+            {filteredWordsHorror.map((movie,index) => (
               <div className="movie" key={index}>
                 <img src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
               </div>
