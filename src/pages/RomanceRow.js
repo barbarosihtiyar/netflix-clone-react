37c17fb8f693471884c66fd113f4b5bc89fe5a20
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-03-31 21:49:12 +0300

    searchMovieRomanceComponent

--- src/pages/RomanceRow.js
+++ src/pages/RomanceRow.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from "../axios"
 import "../sass/romance.css"
+import { MainContext, useContext } from "../context";
+
 
 function RomanceRow({title,fetchURL}) {
   const [romanceMovie,setRomanceMovie] = useState([]);
   const base_URL = "https://image.tmdb.org/t/p/original";
+  let {change,setChange} = useContext(MainContext);
 
   useEffect(() => {
     const fetchRom = async () => {
@@ -13,12 +16,15 @@ function RomanceRow({title,fetchURL}) {
     }
     fetchRom();
     })
+
+    const filteredWordsRomance = romanceMovie.filter(word => word.original_title.toLowerCase().includes(change))
+
   return (
     <div className="romanceRowContainer">
     <div className="romanceRowWrapper">
     <h1>{title}</h1>
       <div className="romance">
-      {romanceMovie.map((movie,index) => (
+      {filteredWordsRomance.map((movie,index) => (
         <div className="movie" key={index}>
           <img src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
         </div>
