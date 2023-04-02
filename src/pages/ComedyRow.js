f446b0e85048de697533f7ae1623fe5b7455ff3b
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-02 18:14:38 +0300

    searcgMovieComedyComponent

--- src/pages/ComedyRow.js
+++ src/pages/ComedyRow.js
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from 'react'
 import "../sass/comedy.css"
 import axios from "../axios"
 import { MainContext, useContext } from "../context";
+import { BsFillInfoCircleFill } from 'react-icons/bs';
 
 function ComedyRow({title,fetchURL}) {
   const [comedy,setComedy] = useState([]);
+  const [showsInfo,setShowsInfo] = useState(false)
   let {change,setChange} = useContext(MainContext);
 
   const filteredWordsComedy = comedy.filter(word => word.original_title.toLowerCase().includes(change))
 
+  const showInfo = (e) => {
+    setShowsInfo(true);
+  }
 
   useEffect(() => {
     async function fetchCom(){
@@ -27,7 +32,12 @@ function ComedyRow({title,fetchURL}) {
           <div className="comedy">
             {filteredWordsComedy.map((movie,index) => (
               <div key={index} className="movie">
+              <div className="image">
               <img src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
+               <div className="absoluteComedy" onClick={showInfo}>
+                <BsFillInfoCircleFill/>
+              </div>
+              </div>
               </div>
             ))}
           </div>
