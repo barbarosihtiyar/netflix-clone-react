c3eb8dd83e5755a5baceca585f88664e6cc51a63
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-09 22:02:31 +0300

    clickMovieInfo

--- src/pages/ActionRow.js
+++ src/pages/ActionRow.js
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import { BsFillInfoCircleFill } from "react-icons/bs";
@@ -14,76 +8,80 @@ import { MainContext, useContext } from "../context";
 
 
 function ActionRow({ fetchURL, title }) {
-const [action, setAction] = useState([]);
-const [showsInfo,setShowsInfo] = useState(false)
-let {change,setChange} = useContext(MainContext);
-const [selectedPhoto,setSelectedPhoto] = useState(null)
+  const [action, setAction] = useState([]);
+  const [showsInfo,setShowsInfo] = useState(false)
+  let {change,setChange} = useContext(MainContext);
+  const [selectedPhoto,setSelectedPhoto] = useState(null)
 
-useEffect(() => {
-  async function fetchAct() {
-    const actReq = await axios.get(fetchURL);
-    setAction(actReq.data.results);
-    console.log(actReq.data.results)
+  useEffect(() => {
+    async function fetchAct() {
+      const actReq = await axios.get(fetchURL);
+      setAction(actReq.data.results);
+      console.log(actReq.data.results)
+    }
+    fetchAct();
+  }, [fetchURL]);
+
+  const showInfo = (key) => {
+    setShowsInfo(!showsInfo);
+    setSelectedPhoto(key)
+    console.log(key)
   }
-  fetchAct();
-}, [fetchURL]);
 
-const showInfo = (key) => {
-  setShowsInfo(!showsInfo);
-  setSelectedPhoto(key)
-  console.log(key)
-}
-
-const filteredWordsAction = action.filter(word => word.original_title.toLowerCase().includes(change))
+  const filteredWordsAction = action.filter(word => word.original_title.toLowerCase().includes(change))
 
 
-const base_URL = "https://image.tmdb.org/t/p/original";
+  const base_URL = "https://image.tmdb.org/t/p/original";
 
-return (
-  <div className="moviesRowContainer">
-    <div className="moviesRowWrapper">
-      <h1>{title}</h1>
-      <div className="movies">
-        {filteredWordsAction.map((movie, index) => (
-          <div key={index} className="movie">
-          <div className="image" >
-          <img src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
-          <div key={index+1}  className="absoluteAction" onClick={() => showInfo(index+1)}>
-              <BsFillInfoCircleFill/>
-            </div>
-          </div>
-          {selectedPhoto &&(
-            <div className="clickPopup" style={showsInfo ? {display:"block"} : {display : "none"}}>
-            <div className="clickPopupContainer">
-              <div className="clickPopupWrapper">
-                <div className="clickPopupImage">
-                <img src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
-                </div>
-                <div className="clickPopupTitleContent">
-                  <div className="clickPopupTitle">
-                  <h1>{movie.title}</h1>
-                  </div>
-                  <div className="clickPopupContent">
-                    <span>Language : {movie.original_language}</span>
-                    <span>Release Date : {movie.release_date}</span>
-                    <span>Vote Average : {movie.vote_average}</span>
-                    <span>Number of people voting : {movie.vote_count}</span>
-                  </div>
-                  <div className="clickPopupSum">
-                    <p>{movie.overview}</p>
-                  </div>
-                </div>
-                div.
+  return (
+    <div className="moviesRowContainer">
+      <div className="moviesRowWrapper">
+        <h1>{title}</h1>
+        <div className="movies">
+          {filteredWordsAction.map((movie, index) => (
+            <div key={index} className="movie">
+            <div className="image" >
+            <img className={index} src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
+            <div key={index+1}  className="absoluteAction" onClick={() => showInfo(index+1)}>
+                <BsFillInfoCircleFill/>
               </div>
             </div>
+            {selectedPhoto &&(
+              <div className="clickPopup" style={showsInfo ? {display:"block"} : {display : "none"}}>
+              <div className="clickPopupContainer">
+                <div className="clickPopupWrapper">
+                  <div className="clickPopupImage">
+                  {selectedPhoto === index+1 ?                   
+                  <img className={index+1} src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
+                    : ""}
+                  </div>
+                  <div className="clickPopupTitleContent">
+                    <div className="clickPopupTitle">
+                    {selectedPhoto === index+1 ?                   
+                      <h1>{movie.title}</h1>
+                    : ""}
+                    </div>
+                    <div className="clickPopupContent">
+                      {selectedPhoto === index+1 ?  <span>Language : {movie.original_language}</span> : ""}
+                      {selectedPhoto === index+1 ?  <span>Release Date : {movie.release_date}</span> : ""}
+                      {selectedPhoto === index+1 ?  <span>Vote Average : {movie.vote_average}</span> : ""}
+                      {selectedPhoto === index+1 ?  <span>Number of people voting : {movie.vote_count}</span> : ""}
+                    </div>
+                    <div className="clickPopupSum">
+                    {selectedPhoto === index+1 ?  <p>{movie.overview}</p> : ""}
+                      
+                    </div>
+                  </div>
+                </div>
+              </div>
+              </div>
+            )}
             </div>
-          )}
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default ActionRow;
