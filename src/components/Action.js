0186031b1499e374f5b84220cc99dc5d5a0461b3
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-11 23:23:14 +0300

    addContextApi for ActionRow

--- src/components/Action.js
+++ src/components/Action.js
@@ -4,13 +4,20 @@ import "../sass/movies.css";
 import requests from "../requests";
 import MoviesRow from "../pages/ActionRow";
 import Header from "../pages/Header";
+import bg from "../sass/fixedBg.module.css"
+import { useState } from "react";
+
 
 function Action() {
+  const [selectedPhoto,setSelectedPhoto] = useState(null);
+
   return (
     <>
+        <div className={selectedPhoto === null ? "" : bg.fixedBg}>
         <Header />
       <Banner fetchURL={requests.fetchActionMovies}/>
-      <MoviesRow title={"Action Movies"} fetchURL={requests.fetchActionMovies}/>
+      <MoviesRow title={"Action Movies"} fetchURL={requests.fetchActionMovies} selectedPhoto={selectedPhoto} setSelectedPhoto={setSelectedPhoto}/>
+      </div>
     </>
   );
 }
