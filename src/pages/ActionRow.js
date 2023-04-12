020e2381232abd12e4ddfe2d09f44935b26021d7
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-12 23:53:41 +0300

    codePrettier

--- src/pages/ActionRow.js
+++ src/pages/ActionRow.js
@@ -3,20 +3,20 @@ import axios from "../axios";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { GrAddCircle } from "react-icons/gr";
-import { AiTwotoneLike } from "react-icons/ai";  
+import { AiTwotoneLike } from "react-icons/ai";
 import { FaTimes } from "react-icons/fa";
 import { MainContext, useContext } from "../context";
 
-function ActionRow({ fetchURL, title,selectedPhoto,setSelectedPhoto  }) {
+function ActionRow({ fetchURL, title, selectedPhoto, setSelectedPhoto }) {
   const [action, setAction] = useState([]);
-  const [showsInfo,setShowsInfo] = useState(false);
-  let {change,setChange} = useContext(MainContext);
+  const [showsInfo, setShowsInfo] = useState(false);
+  let { change, setChange } = useContext(MainContext);
 
   useEffect(() => {
     async function fetchAct() {
       const actReq = await axios.get(fetchURL);
       setAction(actReq.data.results);
-      console.log(actReq.data.results)
+      console.log(actReq.data.results);
     }
     fetchAct();
   }, [fetchURL]);
@@ -25,16 +25,15 @@ function ActionRow({ fetchURL, title,selectedPhoto,setSelectedPhoto  }) {
     setShowsInfo(true);
     setSelectedPhoto(key);
     // console.log(key)
-  }
-
+  };
 
   const closeMovieInfo = () => {
-    setSelectedPhoto(null)
-  }
- 
+    setSelectedPhoto(null);
+  };
 
-  const filteredWordsAction = action.filter(word => word.original_title.toLowerCase().includes(change))
-
+  const filteredWordsAction = action.filter((word) =>
+    word.original_title.toLowerCase().includes(change)
+  );
 
   const base_URL = "https://image.tmdb.org/t/p/original";
 
@@ -45,43 +44,86 @@ function ActionRow({ fetchURL, title,selectedPhoto,setSelectedPhoto  }) {
         <div className="movies">
           {filteredWordsAction.map((movie, index) => (
             <div key={index} className="movie">
-            <div className="image" >
-            <img className={index} src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
-            <div key={index+1}  className="absoluteAction" style={{display:"absolute"}} onClick={() => showInfo(index+1)}>
-                {selectedPhoto === null ? <BsFillInfoCircleFill/> : ""}
+              <div className="image">
+                <img
+                  className={index}
+                  src={`${base_URL}${movie.poster_path}`}
+                  alt={movie.name}
+                />
+                <div
+                  key={index + 1}
+                  className="absoluteAction"
+                  style={{ display: "absolute" }}
+                  onClick={() => showInfo(index + 1)}
+                >
+                  {selectedPhoto === null ? <BsFillInfoCircleFill /> : ""}
+                </div>
               </div>
-            </div>
-            {selectedPhoto &&(
-              <div className="clickPopup" style={showsInfo ? {display:"block"} : {display : "none"}}>
-              <div className="clickPopupContainer">
-                <div className="clickPopupWrapper">
-                  <div className="clickPopupImage">
-                  {selectedPhoto === index+1 ?                   
-                  <img className={index+1} src={`${base_URL}${movie.backdrop_path}`} alt={movie.name} />
-                    : ""}
-                    <FaTimes onClick={closeMovieInfo}/>
-                  </div>
-                  <div className="clickPopupTitleContent">
-                    <div className="clickPopupTitle">
-                    {selectedPhoto === index+1 ?                   
-                      <h1>{movie.title}</h1>
-                    : ""}
-                    </div>
-                    <div className="clickPopupContent">
-                      {selectedPhoto === index+1 ?  <span>Language : {movie.original_language.toUpperCase()}</span> : ""}
-                      {selectedPhoto === index+1 ?  <span>Release Date : {movie.release_date}</span> : ""}
-                      {selectedPhoto === index+1 ?  <span>Vote Average : {movie.vote_average}</span> : ""}
-                      {selectedPhoto === index+1 ?  <span>Number of people voting : {movie.vote_count}</span> : ""}
-                    </div>
-                    <div className="clickPopupSum">
-                    {selectedPhoto === index+1 ?  <p>{movie.overview}</p> : ""}
-                      
+              {selectedPhoto && (
+                <div
+                  className="clickPopup"
+                  style={showsInfo ? { display: "block" } : { display: "none" }}
+                >
+                  <div className="clickPopupContainer">
+                    <div className="clickPopupWrapper">
+                      <div className="clickPopupImage">
+                        {selectedPhoto === index + 1 ? (
+                          <img
+                            className={index + 1}
+                            src={`${base_URL}${movie.backdrop_path}`}
+                            alt={movie.name}
+                          />
+                        ) : (
+                          ""
+                        )}
+                        <FaTimes onClick={closeMovieInfo} />
+                      </div>
+                      <div className="clickPopupTitleContent">
+                        <div className="clickPopupTitle">
+                          {selectedPhoto === index + 1 ? (
+                            <h1>{movie.title}</h1>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div className="clickPopupContent">
+                          {selectedPhoto === index + 1 ? (
+                            <span>
+                              Language : {movie.original_language.toUpperCase()}
+                            </span>
+                          ) : (
+                            ""
+                          )}
+                          {selectedPhoto === index + 1 ? (
+                            <span>Release Date : {movie.release_date}</span>
+                          ) : (
+                            ""
+                          )}
+                          {selectedPhoto === index + 1 ? (
+                            <span>Vote Average : {movie.vote_average}</span>
+                          ) : (
+                            ""
+                          )}
+                          {selectedPhoto === index + 1 ? (
+                            <span>
+                              Number of people voting : {movie.vote_count}
+                            </span>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div className="clickPopupSum">
+                          {selectedPhoto === index + 1 ? (
+                            <p>{movie.overview}</p>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              </div>
-            )}
+              )}
             </div>
           ))}
         </div>
