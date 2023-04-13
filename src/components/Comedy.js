ed0e1b50e15065f999abce280bf8af838670c7ee
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-13 23:53:47 +0300

    contextApi for ComedyRow

--- src/components/Comedy.js
+++ src/components/Comedy.js
@@ -3,13 +3,23 @@ import Banner from "../pages/Banner";
 import ComedyRow from "../pages/ComedyRow";
 import Header from "../pages/Header";
 import requests from "../requests";
+import { useState } from "react";
+import bg from "../sass/fixedBg.module.css";
 
 function Comedy() {
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
   return (
     <>
-      <Header />
-      <Banner fetchURL={requests.fetchComedyMovies} />
-      <ComedyRow title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <div className={selectedPhoto === null ? "" : bg.fixedBg}>
+        <Header />
+        <Banner fetchURL={requests.fetchComedyMovies} />
+        <ComedyRow
+          title="Comedy Movies"
+          fetchURL={requests.fetchComedyMovies}
+          selectedPhoto={selectedPhoto}
+          setSelectedPhoto={setSelectedPhoto}
+        />
+      </div>
     </>
   );
 }
