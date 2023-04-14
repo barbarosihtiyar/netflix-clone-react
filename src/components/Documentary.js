3dc34393f6bb572be365e7b28348f5a2148cd3b0
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-14 20:47:49 +0300

    clickPhoto props for documentaryRow

--- src/components/Documentary.js
+++ src/components/Documentary.js
@@ -3,16 +3,25 @@ import DocumentaryRow from "../pages/DocumentaryRow";
 import Banner from "../pages/Banner";
 import requests from "../requests";
 import Header from "../pages/Header";
+import bg from "../sass/fixedBg.module.css";
+import { useState } from "react";
 
 function Documentary() {
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
   return (
     <>
-      <Header />
-      <Banner fetchURL={requests.fetchDocumentaries} />
-      <DocumentaryRow
-        title={"Documentary Movie"}
-        fetchURL={requests.fetchDocumentaries}
-      />
+      <div className={selectedPhoto === null ? "" : bg.fixedBg}>
+        <Header />
+        <Banner
+          fetchURL={requests.fetchDocumentaries}
+        />
+        <DocumentaryRow
+          title={"Documentary Movie"}
+          fetchURL={requests.fetchDocumentaries}
+          selectedPhoto={selectedPhoto}
+          setSelectedPhoto={setSelectedPhoto}
+        />
+      </div>
     </>
   );
 }
