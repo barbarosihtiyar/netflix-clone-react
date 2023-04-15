8cabdba45983159bb355be320626b785be64390e
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-15 19:25:36 +0300

    styleModuleCss & getSelectedPhotoFunc

--- src/components/Horror.js
+++ src/components/Horror.js
@@ -3,16 +3,23 @@ import Banner from "../pages/Banner";
 import Header from "../pages/Header";
 import HorrorRow from "../pages/HorrorRow";
 import requests from "../requests";
+import { useState } from "react";
+import bg from "../sass/fixedBg.module.css";
 
 function Horror() {
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
   return (
     <>
-      <Header />
-      <Banner fetchURL={requests.fetchHorrorMovies} />
-      <HorrorRow
-        title={"Horror Movies"}
-        fetchURL={requests.fetchHorrorMovies}
-      />
+      <div className={selectedPhoto === null ? "" : bg.fixedBg}>
+        <Header />
+        <Banner fetchURL={requests.fetchHorrorMovies} />
+        <HorrorRow
+          title={"Horror Movies"}
+          fetchURL={requests.fetchHorrorMovies}
+          selectedPhoto={selectedPhoto}
+          setSelectedPhoto={setSelectedPhoto}
+        />
+      </div>
     </>
   );
 }
