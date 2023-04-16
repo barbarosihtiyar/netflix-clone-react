2b4fcc7b79eb780217cd2e4e7f1e16c38032947b
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-16 19:46:42 +0300

    clıckMovıeInfoFunc & Conditions

--- src/pages/RomanceRow.js
+++ src/pages/RomanceRow.js
@@ -1,49 +1,128 @@
-import React, { useEffect, useState } from 'react'
-import axios from "../axios"
-import "../sass/romance.css"
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "../sass/romance.css";
 import { MainContext, useContext } from "../context";
-import { BsFillInfoCircleFill } from 'react-icons/bs';
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { FaTimes } from "react-icons/fa";
 
-
-function RomanceRow({title,fetchURL}) {
-  const [romanceMovie,setRomanceMovie] = useState([]);
+function RomanceRow({ title, fetchURL, selectedPhoto, setSelectedPhoto }) {
+  const [romanceMovie, setRomanceMovie] = useState([]);
   const base_URL = "https://image.tmdb.org/t/p/original";
-  let {change,setChange} = useContext(MainContext);
-  const [showsInfo,setShowsInfo] = useState(false)
+  let { change, setChange } = useContext(MainContext);
+  const [showsInfo, setShowsInfo] = useState(false);
 
   useEffect(() => {
     const fetchRom = async () => {
-      const movies = await axios.get(fetchURL)
+      const movies = await axios.get(fetchURL);
       setRomanceMovie(movies.data.results);
-    }
+    };
     fetchRom();
-    })
+  });
 
-    const filteredWordsRomance = romanceMovie.filter(word => word.original_title.toLowerCase().includes(change))
+  const filteredWordsRomance = romanceMovie.filter((word) =>
+    word.original_title.toLowerCase().includes(change)
+  );
 
-    const showInfo = (e) => {
-      setShowsInfo(true);
-    }
+  const showInfo = (key) => {
+    setShowsInfo(true);
+    setSelectedPhoto(key);
+    // console.log(key)
+  };
+
+  const closeMovieInfo = () => {
+    setSelectedPhoto(null);
+  };
 
   return (
     <div className="romanceRowContainer">
-    <div className="romanceRowWrapper">
-    <h1>{title}</h1>
-      <div className="romance">
-      {filteredWordsRomance.map((movie,index) => (
-        <div className="movie" key={index}>
-        <div className="image">
-        <img src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
-        <div className="absoluteRomance" onClick={showInfo}>
-                <BsFillInfoCircleFill/>
+      <div className="romanceRowWrapper">
+        <h1>{title}</h1>
+        <div className="romance">
+          {filteredWordsRomance.map((movie, index) => (
+            <div className="movie" key={index}>
+              <div className="image">
+                <img src={`${base_URL}${movie.poster_path}`} alt={movie.name} />
+                <div
+                  className="absoluteRomance"
+                  onClick={() => showInfo(index + 1)}
+                >
+                  {selectedPhoto === null ? <BsFillInfoCircleFill /> : ""}
+                </div>
               </div>
+              {selectedPhoto && (
+                <div
+                  className="clickPopup"
+                  style={showsInfo ? { display: "block" } : { display: "none" }}
+                >
+                  <div className="clickPopupContainer">
+                    <div className="clickPopupWrapper">
+                      <div className="clickPopupImage">
+                        {selectedPhoto === index + 1 ? (
+                          <img
+                            className={index + 1}
+                            src={`${base_URL}${movie.backdrop_path}`}
+                            alt={movie.name}
+                          />
+                        ) : (
+                          ""
+                        )}
+                        <FaTimes
+                          onClick={closeMovieInfo}
+                          style={{ cursor: "pointer" }}
+                        />
+                      </div>
+                      <div className="clickPopupTitleContent">
+                        <div className="clickPopupTitle">
+                          {selectedPhoto === index + 1 ? (
+                            <h1>{movie.title}</h1>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div className="clickPopupContent">
+                          {selectedPhoto === index + 1 ? (
+                            <span>
+                              Language : {movie.original_language.toUpperCase()}
+                            </span>
+                          ) : (
+                            ""
+                          )}
+                          {selectedPhoto === index + 1 ? (
+                            <span>Release Date : {movie.release_date}</span>
+                          ) : (
+                            ""
+                          )}
+                          {selectedPhoto === index + 1 ? (
+                            <span>Vote Average : {movie.vote_average}</span>
+                          ) : (
+                            ""
+                          )}
+                          {selectedPhoto === index + 1 ? (
+                            <span>
+                              Number of people voting : {movie.vote_count}
+                            </span>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                        <div className="clickPopupSum">
+                          {selectedPhoto === index + 1 ? (
+                            <p>{movie.overview}</p>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          ))}
         </div>
-        </div>
-      ))}
       </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default RomanceRow
+export default RomanceRow;
