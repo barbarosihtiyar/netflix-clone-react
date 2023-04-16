265619cc0001881b24e86577a3c20558c60c1be6
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-16 19:46:09 +0300

    created props for RomanceRow & moduleCss

--- src/components/Romance.js
+++ src/components/Romance.js
@@ -3,16 +3,23 @@ import Banner from "../pages/Banner";
 import Header from "../pages/Header";
 import RomanceRow from "../pages/RomanceRow";
 import requests from "../requests";
+import { useState } from "react";
+import bg from "../sass/fixedBg.module.css";
 
 function Romance() {
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
   return (
     <>
+      <div className={selectedPhoto === null ? "" : bg.fixedBg}>
       <Header />
       <Banner fetchURL={requests.fetchRomanceMovies} />
       <RomanceRow
         title="Romance Movies"
         fetchURL={requests.fetchRomanceMovies}
+        selectedPhoto={selectedPhoto}
+        setSelectedPhoto={setSelectedPhoto}
       />
+      </div>
     </>
   );
 }
