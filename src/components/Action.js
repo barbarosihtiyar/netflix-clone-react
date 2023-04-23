44d04a911d0e153996a04f4ca4c674d4a14856e4
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-23 15:38:37 +0300

    createdLoadingButtons

--- src/components/Action.js
+++ src/components/Action.js
@@ -10,14 +10,16 @@ import { useState } from "react";
 
 function Action() {
   const [selectedPhoto,setSelectedPhoto] = useState(null);
+  const [isLoading,setIsLoading] = useState(false);
 
   return (
     <>
+        {isLoading ? <LoadingButtons /> : ""}
         <div className={selectedPhoto === null ? "" : bg.fixedBg}>
         <Header />
       <Banner fetchURL={requests.fetchActionMovies}/>
-      <MoviesRow title={"Action Movies"} fetchURL={requests.fetchActionMovies} selectedPhoto={selectedPhoto} setSelectedPhoto={setSelectedPhoto}/>
-      </div>
+      <MoviesRow title={"Action Movies"} fetchURL={requests.fetchActionMovies} selectedPhoto={selectedPhoto} 
+      setSelectedPhoto={setSelectedPhoto} setIsLoading={setIsLoading} isLoading={isLoading}/>      </div>
     </>
   );
 }
