f0e43df3672d0036130da613bb93186c368dffb4
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-25 22:21:08 +0300

    LoadingButton ComedyComponent

--- src/components/Comedy.js
+++ src/components/Comedy.js
@@ -5,12 +5,16 @@ import Header from "../pages/Header";
 import requests from "../requests";
 import { useState } from "react";
 import bg from "../sass/fixedBg.module.css";
+import LoadingButtons from "../pages/LoadingButtons";
 
 function Comedy() {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [isLoading,setIsLoading] = useState(false)
+
   return (
     <>
       <div className={selectedPhoto === null ? "" : bg.fixedBg}>
+      {isLoading ? <LoadingButtons /> : ""}
         <Header />
         <Banner fetchURL={requests.fetchComedyMovies} />
         <ComedyRow
@@ -18,6 +22,8 @@ function Comedy() {
           fetchURL={requests.fetchComedyMovies}
           selectedPhoto={selectedPhoto}
           setSelectedPhoto={setSelectedPhoto}
+          setIsLoading={setIsLoading} 
+          isLoading={isLoading}
         />
       </div>
     </>
