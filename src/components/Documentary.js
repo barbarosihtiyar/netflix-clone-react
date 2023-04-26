f32530f9d2373697bc561ded8c6d56c1d4272e88
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-26 16:48:53 +0300

    LoadingButton DocumentaryComponent

--- src/components/Documentary.js
+++ src/components/Documentary.js
@@ -5,21 +5,25 @@ import requests from "../requests";
 import Header from "../pages/Header";
 import bg from "../sass/fixedBg.module.css";
 import { useState } from "react";
+import LoadingButtons from "../pages/LoadingButtons";
 
 function Documentary() {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <>
       <div className={selectedPhoto === null ? "" : bg.fixedBg}>
+        {isLoading ? <LoadingButtons /> : ""}
         <Header />
-        <Banner
-          fetchURL={requests.fetchDocumentaries}
-        />
+        <Banner fetchURL={requests.fetchDocumentaries} />
         <DocumentaryRow
           title={"Documentary Movie"}
           fetchURL={requests.fetchDocumentaries}
           selectedPhoto={selectedPhoto}
           setSelectedPhoto={setSelectedPhoto}
+          setIsLoading={setIsLoading}
+          isLoading={isLoading}
         />
       </div>
     </>
