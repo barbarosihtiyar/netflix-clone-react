0c006a2d58e30f99723b1550b25fbd3ad740e22d
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-04-26 16:49:03 +0300

    LoadingButton RomanceComponent

--- src/components/Romance.js
+++ src/components/Romance.js
@@ -5,20 +5,26 @@ import RomanceRow from "../pages/RomanceRow";
 import requests from "../requests";
 import { useState } from "react";
 import bg from "../sass/fixedBg.module.css";
+import LoadingButtons from "../pages/LoadingButtons";
 
 function Romance() {
   const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <>
       <div className={selectedPhoto === null ? "" : bg.fixedBg}>
-      <Header />
-      <Banner fetchURL={requests.fetchRomanceMovies} />
-      <RomanceRow
-        title="Romance Movies"
-        fetchURL={requests.fetchRomanceMovies}
-        selectedPhoto={selectedPhoto}
-        setSelectedPhoto={setSelectedPhoto}
-      />
+        {isLoading ? <LoadingButtons /> : ""}
+        <Header />
+        <Banner fetchURL={requests.fetchRomanceMovies} />
+        <RomanceRow
+          title="Romance Movies"
+          fetchURL={requests.fetchRomanceMovies}
+          selectedPhoto={selectedPhoto}
+          setSelectedPhoto={setSelectedPhoto}
+          setIsLoading={setIsLoading}
+          isLoading={isLoading}
+        />
       </div>
     </>
   );
