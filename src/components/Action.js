98f20420581b220457c547c39fbdac54c84b50bb
Barbaros İhtiyar <barbarossihtiyar@gmail.com>
2023-05-02 00:04:00 +0300

    importCss File / add LoadingButtons / cleanCode

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,25 +1,33 @@
 import React from "react";
 import Banner from "../pages/Banner";
+import LoadingButtons from "../pages/LoadingButtons";
 import "../sass/movies.css";
 import requests from "../requests";
 import MoviesRow from "../pages/ActionRow";
 import Header from "../pages/Header";
-import bg from "../sass/fixedBg.module.css"
+import bg from "../sass/fixedBg.module.css";
 import { useState } from "react";
-
+import "../sass/favorite.css"
 
 function Action() {
-  const [selectedPhoto,setSelectedPhoto] = useState(null);
-  const [isLoading,setIsLoading] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <>
+      <div className={selectedPhoto === null ? "" : bg.fixedBg}>
         {isLoading ? <LoadingButtons /> : ""}
-        <div className={selectedPhoto === null ? "" : bg.fixedBg}>
         <Header />
-      <Banner fetchURL={requests.fetchActionMovies}/>
-      <MoviesRow title={"Action Movies"} fetchURL={requests.fetchActionMovies} selectedPhoto={selectedPhoto} 
-      setSelectedPhoto={setSelectedPhoto} setIsLoading={setIsLoading} isLoading={isLoading}/>      </div>
+        <Banner fetchURL={requests.fetchActionMovies} />
+        <MoviesRow
+          title={"Action Movies"}
+          fetchURL={requests.fetchActionMovies}
+          selectedPhoto={selectedPhoto}
+          setSelectedPhoto={setSelectedPhoto}
+          setIsLoading={setIsLoading}
+          isLoading={isLoading}
+        />
+      </div>
     </>
   );
 }
